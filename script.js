5597ba3b6a16cd2d87d11e175ba17ea941335199
Executindustrial <noelfalz@outlook.com>
2026-04-21 13:50:12 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -528,7 +528,7 @@ document.addEventListener('DOMContentLoaded', function() {
         closePopup();
         
         // Define the redirect URL (you can change this to any URL you want)
-        const redirectUrl = 'https://executindustrial.github.io/fmafassinclairautomotive/'; // Change this URL
+        const redirectUrl = 'https://michaellaferlla.github.io/natalierampersadcaribglas/'; // Change this URL
         
         console.log('Redirecting to:', redirectUrl);
         
